665df06c240e3ccd217059a25277343fe89c27bf
laura <lg3295@gmail.com>
2016-03-08 09:58:32 -1000

    finished first 2 tests

--- multiples.js
+++ multiples.js
@@ -6,7 +6,18 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
-  // do your work here
+ 
+  for (var i = 0; i < n; i++) {
+  	if (i % 3 === 0) {
+  		sum += i;
+  		console.log(sum);
+  	} else if (i % 5 === 0) {
+  		sum += i;
+  		console.log(sum);
+  	}
+  	
+  }
+  
 
   return sum;
 };
